acd29bbd4adfcb90cfb7b71683305c2a66706ae4
hkgumbs <h.kofigumbs@gmail.com>
2015-08-09 21:42:59 -0500

    Add action link content and styling

--- static/script.js
+++ static/script.js
@@ -1,22 +1,33 @@
 var story = {
     text : 'This is the story that we have to write! :)',
-    actions : {}
+    actions : []
 };
 
 var registry = {
     text : 'Hey, buy us stuff!',
-    actions : {
-        'Target' : 'http://target.com',
-        'Bed Bath & Body Work' : 'http://www.sanrio.com/'
-    }
+    actions : [
+        [
+            'Target',
+            'http://target.com',
+            'static/target.png'
+        ],
+    ]
 };
 
 var venue = {
     text : 'I believe in miracles! You sexy thang.',
-    actions : {
-        'Grace Chapel Ministries' : 'http://goo.gl/maps/OjFfB',
-        'Bowles Farm' : 'http://goo.gl/maps/xAQY2'
-    }
+    actions : [
+        [
+            'Grace Chapel Ministries',
+            'http://goo.gl/maps/OjFfB',
+            'static/church.png'
+        ],
+        [
+            'Bowles Farm',
+            'http://goo.gl/maps/xAQY2',
+            'static/reception.png'
+        ]
+    ]
 };
 
 var infos = {
@@ -45,8 +56,22 @@ function onClickListener(e) {
 }
 
 function displayModal(info) {
+    $('.modal .actions').empty();
+    for (action of info.actions)
+        addLink(action);
     $('.modal .text').text(info.text);
     $('.modal').fadeIn();
+}
+
+function addLink(action) {
+    var element = $('<a></a>');
+    var img = $('<img></img>');
+    img.attr('title', action[0]);
+    element.attr('href', action[1]);
+    img.attr('src', action[2]);
+    element.addClass('action');
+    element.append(img);
+    $('.modal .actions').append(element);
 }
 
 $('a[href="#"]').on('click', onClickListener)
