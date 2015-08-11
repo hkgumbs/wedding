e11e6cdd3832b474292609808c3549e685a7c862
hkgumbs <h.kofigumbs@gmail.com>
2015-08-10 20:52:33 -0500

    Emphasize links

--- static/script.js
+++ static/script.js
@@ -1,77 +1,16 @@
-var story = {
-    text : 'This is the story that we have to write! :)',
-    actions : []
+var text = {
+    'story' : 'This is the story that we have to write! :)',
+    'registry' : 'Hey, buy us stuff!',
+    'venue' : 'I believe in miracles! You sexy thang.',
 };
-
-var registry = {
-    text : 'Hey, buy us stuff!',
-    actions : [
-        [
-            'Target',
-            'http://target.com',
-            'static/target.png'
-        ],
-    ]
-};
-
-var venue = {
-    text : 'I believe in miracles! You sexy thang.',
-    actions : [
-        [
-            'Grace Chapel Ministries',
-            'http://goo.gl/maps/OjFfB',
-            'static/church.png'
-        ],
-        [
-            'Bowles Farm',
-            'http://goo.gl/maps/xAQY2',
-            'static/reception.png'
-        ]
-    ]
-};
-
-var infos = {
-    "story" : story,
-    "registry" : registry,
-    "venue" : venue
-}
 
 function onClickListener(e) {
-    var info;
-    switch ($(this).attr('value')) {
-        case 'story':
-            info = story;
-            break;
-        case 'registry':
-            info = registry;
-            break;
-        case 'venue':
-            info = venue;
-            break;
-        default:
-            $('.modal').fadeOut();
-            return;
-    }
-    displayModal(info);
-}
-
-function displayModal(info) {
-    $('.modal .actions').empty();
-    for (action of info.actions)
-        addLink(action);
-    $('.modal .text').text(info.text);
-    $('.modal').fadeIn();
-}
-
-function addLink(action) {
-    var element = $('<a></a>');
-    var img = $('<img></img>');
-    img.attr('title', action[0]);
-    element.attr('href', action[1]);
-    img.attr('src', action[2]);
-    element.addClass('action');
-    element.append(img);
-    $('.modal .actions').append(element);
+    var value = $(this).attr('value');
+    if (value) {
+        $('.modal .text').text(text[value]);
+        $('.modal').fadeIn();
+    } else
+        $('.modal').fadeOut();
 }
 
 $('a[href="#"]').on('click', onClickListener)
